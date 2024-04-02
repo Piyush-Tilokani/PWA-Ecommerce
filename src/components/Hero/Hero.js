2ce3f3f976ee834cf977c3c1489c53d172d3b598
Piyush Tilokani <2021.piyush.tilokani@ves.ac.in>
2024-04-02 16:55:44 +1100

    commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='overlay'></div>
+            <div>
+                <h1 style={{fontSize: 80}}>Shopify</h1>
+                {/* <h2><span style={{ fontWeight: 'bold', color: 'orangered' }}>Front</span> End Developer</h2>
+                <p>E_Commerce React</p> */}
+            </div>
+        </div>
+    )
+}
+
+export default Hero
